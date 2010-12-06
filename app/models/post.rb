cb245b03b371f2da42b2792851d86ac562b26737
mitul <mitul.list@gmail.com>
2010-12-05 20:44:20 -0800

    comments commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+ 
+  has_many :comments
 end

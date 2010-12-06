8d11a8dcc6063c2e025fee54c4f02477cd28b3ee
mitul <mitul.list@gmail.com>
2010-12-05 19:02:41 -0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8dee7adc64791208702a8accf3860ba2bf2301355306b2c70cb0cd76d8962ff836528704c21a68dcd245d948304f844a5cda59829860c55f395acfde537556f8'
